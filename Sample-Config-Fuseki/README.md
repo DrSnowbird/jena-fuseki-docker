6926a3511b30478fb8dfe3a85bfc0dbb089d606c
DrSnowbird <DrSnowbird@openkbs.org>
2022-02-22 23:20:24 -0500

    added Sample-Config-Fuseki folder with example Fuseki config and run shell scripts

---
+++ Sample-Config-Fuseki/README.md
@@ -0,0 +1,6 @@
+# Apache Jena Fuseki
+To execute the evaluation **Apache Jena Fuseki 3.7.0** (or v4.4.0 but not tested on it yet) is required and need to be present in this directory to run local instances of Jena Fuseki.
+Download the version [3.7.0](http://archive.apache.org/dist/jena/binaries/apache-jena-fuseki-3.7.0.zip) directly or look in the [archive](http://archive.apache.org/dist/jena/binaries/).
+* Note: latest version Apache Jean Fuseki:
+  * zip: https://dlcdn.apache.org/jena/binaries/apache-jena-fuseki-4.4.0.zip
+  * tgz: https://dlcdn.apache.org/jena/binaries/apache-jena-fuseki-4.4.0.tar.gz
---
+++ Sample-Config-Fuseki/run_fuseki_growing_field_server.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+# Color codes via https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux#5947802
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+log() {
+  # echo in color
+  echo -e "${GREEN}${1}${NC}"
+}
+
+rlog() {
+  # echo in color
+  echo -e "${RED}${1}${NC}"
+}
+
+RUN_LATEST_FUSEKI=1
+if [ ${RUN_LATEST_FUSEKI} -lt 1 ]; then
+    ## -- Apache Jean Fuseki v3.7.0: -- ##
+    export FUSEKI_HOME="${PWD}/apache-jena-fuseki-3.7.0"
+else
+    ## -- Apache Jean Fuseki v4.4.0: -- ##
+    export FUSEKI_HOME="${PWD}/apache-jena-fuseki-4.4.0"
+fi
+log "Starting Fuseki server in the background"
+${FUSEKI_HOME}/fuseki-server --config=fuseki_config_growing_field.ttl > fuseki.log &
+    
+log "Interface running at ${HOST}"
+
+sleep 8s
+
+FBIN="${FUSEKI_HOME}/bin"
+HOST="http://0.0.0.0:3030"
+#${FBIN}/s-put ${HOST}/dataset/data default data/raw/persons_and_cars.ttl
+
+#${FBIN}/s-put ${HOST}/foaf/data default data/raw/foaf_person.ttl
+log "Loaded Schema.org encoded data into named graph default"
+
+wait
